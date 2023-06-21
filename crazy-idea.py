35b932b0114036f3dc732251cf48427cba5b7731
Xeno852 <lukeblackwell@protonmail.com>
2023-06-20 21:01:06 -0400

    first draft

--- crazy-idea.py
+++ crazy-idea.py
@@ -30,16 +30,28 @@ def find_correspondences(image1, image2):
     keypoints2, descriptors2 = sift.detectAndCompute(gray2, None)
     
     # Initialize matcher
+def find_correspondences(image1, image2):
+    # Convert images to grayscale
+    gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+    
+    # Initialize feature detector and descriptor
+    sift = cv2.SIFT_create()
+    
+    # Detect and compute keypoints and descriptors
+    keypoints1, descriptors1 = sift.detectAndCompute(gray1, None)
+    keypoints2, descriptors2 = sift.detectAndCompute(gray2, None)
+    
+    # Initialize matcher
     matcher = cv2.BFMatcher(cv2.NORM_L2)
     
-    # Sort matches based on distance
-    matches = sorted(matches, key=lambda x: x.distance)
-
+    # Match keypoints and find the two nearest neighbors for each keypoint
+    matches = matcher.knnMatch(descriptors1, descriptors2, k=2)
+    
     # Filter matches using Lowe's ratio test
     ratio_threshold = 0.75
-    good_matches = [first for first, second in zip(matches, matches[1:]) if first.distance < ratio_threshold * second.distance]
-
-        
+    good_matches = [first for first, second in matches if first.distance < ratio_threshold * second.distance]
+    
     # Extract corresponding points
     points1 = np.float32([keypoints1[match.queryIdx].pt for match in good_matches])
     points2 = np.float32([keypoints2[match.trainIdx].pt for match in good_matches])
@@ -87,18 +99,29 @@ if __name__ == '__main__':
     # Extract frames from a video
     frames = extract_frames('path_to_your_video.mp4')
     
-    # Take two consecutive frames as stereo images
-    image1 = frames[0]
-    image2 = frames[1]
-    
-    # Find correspondences between images
-    points1, points2 = find_correspondences(image1, image2)
-    
-    # Calculate disparity map
-    disparity_map = calculate_disparity_map(image1, image2)
-    
-    # Reconstruct 3D points
-    points_3d = reconstruct_3d_points(disparity_map)
+    # Placeholder for 3D points accumulated from all frames
+    all_points_3d = None
+
+    # Loop through pairs of consecutive frames
+    for i in range(len(frames) - 1):
+        # Take two consecutive frames as stereo images
+        image1 = frames[i]
+        image2 = frames[i + 1]
+        
+        # Find correspondences between images
+        points1, points2 = find_correspondences(image1, image2)
+        
+        # Calculate disparity map
+        disparity_map = calculate_disparity_map(image1, image2)
+        
+        # Reconstruct 3D points
+        points_3d = reconstruct_3d_points(disparity_map)
+
+        # Accumulate 3D points
+        if all_points_3d is None:
+            all_points_3d = points_3d
+        else:
+            all_points_3d = np.concatenate((all_points_3d, points_3d), axis=0)
     
     # Export to .obj file
-    export_points_to_obj(points_3d, 'output.obj')
+    export_points_to_obj(all_points_3d, 'output.obj')
